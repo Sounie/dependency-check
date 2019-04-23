14031a6317a3245cef53004f7edc75d3a2b1de31
Stephen Souness <sounie@hotmail.com>
2019-04-23 22:46:11 +0100

    Migrating to Junit5.

--- src/test/java/sounie/WordCountTest.java
+++ src/test/java/sounie/WordCountTest.java
@@ -1,9 +1,10 @@
 package sounie;
 
 import com.google.common.collect.Sets;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 public class WordCountTest {
     @Test
